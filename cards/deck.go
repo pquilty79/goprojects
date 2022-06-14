213ec80b360a2f03998e23b1c9e77079ea107ec8
Patrick Quilty <pquilty79@gmail.com>
2022-06-14 18:54:58 -0500

    write to file and read from file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+
 
 type deck []string
 
@@ -26,3 +33,23 @@ func (d deck) print() {
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
+
+func (d deck)deckToString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck)saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.deckToString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+
