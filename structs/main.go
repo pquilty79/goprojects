3e9daf8e457cea7a1ff9cfbf34cb959051d2ff7a
Patrick Quilty <pquilty79@gmail.com>
2022-06-15 12:08:26 -0500

    structs in go

---
+++ structs/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contact contactInfo
+}
+
+func main() {
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+	// alex.firstName = "Alexander"
+	// fmt.Printf("%+v", alex)
+	jim := person {
+		firstName: "James",
+		lastName: "Adams",
+		contact: contactInfo{
+			email: "jamesadams@gmail.com",
+			zipCode: 12345,
+		},
+	}
+	//pointers are needed in go to find the copy renamed "Jim"
+	jim.updateName("Jim")
+	jim.printPerson()
+}
+
+func (p person) printPerson() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updateName(newFirstName string) {
+     p.firstName = newFirstName
+}
